45d8381601ef66411a318dbfdaa414d362dab05c
Lance Newman <lnewman@brightidea.com>
2025-07-22 17:15:34 -0400

    Bug fix?

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -135,7 +135,7 @@ class QuizController extends Controller
             $geoData['lat'] = (float)$quiz->lat;
 
             $quiz->geo_data = $geoData;
-            $startOfDay = new Carbon($quiz->created_at)->startOfDay();
+            $startOfDay = Carbon::now($quiz->created_at)->startOfDay();
             $quiz->reveal_answer = Carbon::now()->diffInDays($startOfDay) > 0;
 
             $lng = $answer->lng;
