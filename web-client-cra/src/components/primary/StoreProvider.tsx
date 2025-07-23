733a2014cd8e00543a78d04e160db9663a98b654
Lance Newman <lnewman@brightidea.com>
2025-07-22 21:04:36 -0400

    Ran production build and removed logger from main branch for production environments

--- web-client-cra/src/components/primary/StoreProvider.tsx
+++ web-client-cra/src/components/primary/StoreProvider.tsx
@@ -1,25 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { createLogger } from "redux-logger"
 import { ReactNode } from "react"
 import { Provider } from "react-redux"
 import rootReducer from "@reducers/root"
-
-const logger = createLogger({
-    collapsed: true,
-    duration: true,
-    timestamp: true
-})
 
 const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => {
         const middleware = getDefaultMiddleware({ serializableCheck: false })
-        if (process.env.NODE_ENV === "development") {
-            return middleware.concat(logger)
-        }
         return middleware
     },
-    devTools: process.env.NODE_ENV === "development"
+    devTools: false
 })
 
 type ThemeProps = {
